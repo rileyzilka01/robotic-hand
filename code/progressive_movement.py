33dde3a26870b797292bc98905d9b9b2e3f65f49
riley zilka <rileyzilka33@gmail.com>
2022-12-05 23:20:24 +0000

    progressive_movement

--- code/progressive_movement.py
+++ code/progressive_movement.py
@@ -15,12 +15,20 @@ pca.frequency = 50
 servo0 = servo.Servo(pca.channels[0], min_pulse=500, max_pulse=2500)
 servo1 = servo.Servo(pca.channels[1], min_pulse=500, max_pulse=2500)
 
-for i in range(0, 120):
-    servo0.angle = 180-i
+for i in range(0, 100):
+    servo0.angle -= 1
     time.sleep(0.02)
 
 for i in range(0, 180):
-    servo1.angle = i
+    servo1.angle += 1
+    time.sleep(0.02)
+
+while servo0.angle < 179:
+    servo0.angle += 1
+    time.sleep(0.02)
+
+while servo1.angle > 1:
+    servo1.angle -= 1
     time.sleep(0.02)
 
 servo0.angle = 180
