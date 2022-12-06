756f7e69a5b6d125d898491565d7be86aacd51fc
riley zilka <rileyzilka33@gmail.com>
2022-12-06 03:16:35 +0000

    Hand class started

--- code/zeroing_servos.py
+++ code/zeroing_servos.py
@@ -12,10 +12,17 @@ pca = PCA9685(i2c)
 
 pca.frequency = 50
 
-servo0 = servo.Servo(pca.channels[0], min_pulse=500, max_pulse=2500)
-servo1 = servo.Servo(pca.channels[1], min_pulse=500, max_pulse=2500)
+thumbBase = servo.Servo(pca.channels[0], min_pulse=500, max_pulse=2500)
+thumbEnd = servo.Servo(pca.channels[1], min_pulse=500, max_pulse=2500)
+pointerBase = servo.Servo(pca.channels[2], min_pulse=500, max_pulse=2500)
+pointerEnd = servo.Servo(pca.channels[3], min_pulse=500, max_pulse=2500)
+middleBase = servo.Servo(pca.channels[4], min_pulse=500, max_pulse=2500)
+middleEnd = servo.Servo(pca.channels[5], min_pulse=500, max_pulse=2500)
+ringBase = servo.Servo(pca.channels[6], min_pulse=500, max_pulse=2500)
+ringEnd = servo.Servo(pca.channels[7], min_pulse=500, max_pulse=2500)
+pinkyBase = servo.Servo(pca.channels[8], min_pulse=500, max_pulse=2500)
+pinkyEnd = servo.Servo(pca.channels[9], min_pulse=500, max_pulse=2500)
 
-servo0.angle = 180 #Spins the opposite way from servo 1, therefore 0 is essentially 180
-servo1.angle = 0
+
 
 pca.deinit()
