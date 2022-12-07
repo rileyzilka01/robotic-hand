8e19b6e2bc9c62894d548adfa488d6af7d9887e7
Riley Zilka <rileyzilka33@gmail.com>
2022-12-06 19:40:15 -0700

    server done

--- code/server.py
+++ code/server.py
@@ -1,63 +1,82 @@
 
 import socket
-import time
 from queue import Queue
 import cv2 as cv
 import mediapipe as mp
 import numpy as np
 import math
 
-# This class handles the Server side of the comunication between the laptop and the brick.
 class Server:
     def __init__(self, host, port):
-       # setup server socket
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        # We need to use the ip address that shows up in ipconfig for the usb ethernet adapter that handles the comunication between the PC and the brick
         print("Setting up Server\nAddress: " + host + "\nPort: " + str(port))
-        
         serversocket.bind((host, port))
-        # queue up to 5 requests
-        serversocket.listen(1) 
+
+        #5 items in the queue
+        serversocket.listen(5) 
         self.cs, addr = serversocket.accept()
         print ("Connected to: " + str(addr))
 
-    # Sends set of angles to the brick via TCP.
-    # Input: base_angle [Float]: The angle by which we want the base to move
-    #        joint_angle [Float]: The angle by which we want to joint to move
-    #        queue [Thread-safe Queue]: Mutable data structure to store (and return) the messages received from the client
-    def sendAngle(self, base_angle, queue):
-        # Format in which the client expects the data: "angle1,angle2"
-        data = str(base_angle)
-        print("Sending Data: (" + data + ") to robot.")
-        self.cs.send(data.encode("UTF-8"))
-        # Waiting for the client (ev3 brick) to let the server know that it is done moving
+    def sendAngle(self, angles, queue):
+        thumb = str(angles[0])
+        pointer = str(angles[1])
+        middle = str(angles[2])
+        ring = str(angles[3])
+        pinky = str(angles[4])
+        #print(f"Sending Data: [{thumb}, {pointer}, {middle}, {ring}, {pinky}] to robot.")
+        self.cs.send(thumb.encode("UTF-8"))
+        # Waiting for the client to send a response to the server
+        reply = self.cs.recv(128).decode("UTF-8")
+        queue.put(reply)
+        self.cs.send(pointer.encode("UTF-8"))
+        # Waiting for the client to send a response to the server
+        reply = self.cs.recv(128).decode("UTF-8")
+        queue.put(reply)
+        self.cs.send(middle.encode("UTF-8"))
+        # Waiting for the client to send a response to the server
+        reply = self.cs.recv(128).decode("UTF-8")
+        queue.put(reply)
+        self.cs.send(ring.encode("UTF-8"))
+        # Waiting for the client to send a response to the server
+        reply = self.cs.recv(128).decode("UTF-8")
+        queue.put(reply)
+        self.cs.send(pinky.encode("UTF-8"))
+        # Waiting for the client to send a response to the server
         reply = self.cs.recv(128).decode("UTF-8")
         queue.put(reply)
 
-    # Sends set of angles to the brick via TCP.
-    # Input: x [Int]: The x of the end effector
-    #        y [Int]: The y of the end effector
-    #        queue [Thread-safe Queue]: Mutable data structure to store (and return) the messages received from the client
-    def sendPoint(self, x, y, queue):
-        # Format in which the client expects the data: "x,y"
-        data = str(x) + "," + str(y)
-        print("Sending Data: (" + data + ") to robot.")
-        self.cs.send(data.encode("UTF-8"))
-        # Waiting for the client (ev3 brick) to let the server know that it is done moving
-        reply = self.cs.recv(128).decode("UTF-8")
-        queue.put(reply)
+def getAngles(coords, fingerBase, fingerEnd):
+    #This function calculates the angles between the joints provided
 
-    # Sends a termination message to the client. This will cause the client to exit "cleanly", after stopping the motors.
-    def sendTermination(self):
-        self.cs.send("EXIT".encode("UTF-8"))
-
-    # Lets the client know that it should enable safety mode on its end
-    def sendEnableSafetyMode(self):
-        self.cs.send("SAFETY_ON".encode("UTF-8"))
+    #Calculate Base Angle
+    vec1 = [coords[fingerBase[1]].x - coords[fingerBase[0]].x, coords[fingerBase[1]].y - coords[fingerBase[0]].y, coords[fingerBase[1]].z - coords[fingerBase[0]].z]
+    vec2 = [coords[fingerBase[2]].x - coords[fingerBase[1]].x, coords[fingerBase[2]].y - coords[fingerBase[1]].y, coords[fingerBase[2]].z - coords[fingerBase[1]].z]
+    numer = np.dot(vec1, vec2)
+    denom = np.linalg.norm(vec1) * np.linalg.norm(vec2)
+    angle = math.acos(numer/denom)*(180/math.pi)
+    if angle > 90:
+        angle = 90
+    if angle < 0:
+        angle = 0
     
-    # Lets the client know that it should disable safety mode on its end
-    def sendDisableSafetyMode(self):
-        self.cs.send("SAFETY_OFF".encode("UTF-8"))
+    ratio = angle/90
+    baseAngle = 120*ratio
+
+    #Calculate End Angle
+    vec1 = [coords[fingerEnd[1]].x - coords[fingerEnd[0]].x, coords[fingerEnd[1]].y - coords[fingerEnd[0]].y, coords[fingerEnd[1]].z - coords[fingerEnd[0]].z]
+    vec2 = [coords[fingerEnd[2]].x - coords[fingerEnd[1]].x, coords[fingerEnd[2]].y - coords[fingerEnd[1]].y, coords[fingerEnd[2]].z - coords[fingerEnd[1]].z]
+    numer = np.dot(vec1, vec2)
+    denom = np.linalg.norm(vec1) * np.linalg.norm(vec2)
+    angle = math.acos(numer/denom)*(180/math.pi)
+    if angle > 90:
+        angle = 90
+    if angle < 0:
+        angle = 0
+    
+    ratio = angle/90
+    endAngle = 180*ratio
+
+    return baseAngle, endAngle
 
 def main():
     #Server
@@ -65,7 +84,6 @@ def main():
     port = 9999
     server = Server(host, port)
     queue = Queue()
-
 
     #Camera
     cap = cv.VideoCapture(0)
@@ -80,8 +98,15 @@ def main():
         model_complexity=1,
         min_detection_confidence=0.75,
         min_tracking_confidence=0.75,
-        max_num_hands=2
+        max_num_hands=1
     )
+
+    #The point sets for each finger (to get the angles)
+    pointSets = [[[1, 2, 3], [2, 3, 4]],        #Thumb
+                 [[0, 5, 6], [5, 6, 7]],        #Pointer
+                 [[0, 9, 10], [9, 10, 11]],     #Middle
+                 [[0, 13, 14], [13, 14, 15]],   #Ring
+                 [[0, 17, 18], [17, 18, 19]]]   #Pinky
 
     while (True):
         #Get frame from webcam
@@ -96,33 +121,15 @@ def main():
         if results.multi_hand_landmarks:
             for handCoord in results.multi_hand_landmarks:
                 coords = handCoord.landmark
-                print(coords[0].x*w)
-                vec1 = [coords[17].x - coords[0].x, coords[17].y - coords[0].y, coords[17].z - coords[0].z]
-                vec2 = [coords[18].x - coords[17].x, coords[18].y - coords[17].y, coords[18].z - coords[17].z]
-                numer = np.dot(vec1, vec2)
-                denom = np.linalg.norm(vec1) * np.linalg.norm(vec2)
-                angle = math.acos(numer/denom)*(180/math.pi)
-                if angle > 90:
-                    angle = 90
-                if angle < 0:
-                    angle = 0
-                
-                ratio = angle/90
-                conversion = 120*ratio
+                angles = []
+                for i in range(0, len(pointSets)):
+                    baseAngle, endAngle = getAngles(coords, pointSets[i][0], pointSets[i][1])
+                    angles.append([baseAngle, endAngle])
 
-                #Send angle to the pi
-                server.sendAngle(conversion, queue)
+                #Send the list of angles to the pi
+                server.sendAngle(angles, queue)
 
-                print(f'Pinky Base Angle: {angle}')
-                #print(vec1)
-                #for id, coord in enumerate(handCoord.landmark):
-                #    if id == 0:
-                #         cy = int(h*coord.y)
-                #         cx = int(w*coord.x)
-                #         #print(f"{id}: x={cx}, y={cy}")
-                #         cv.circle(img, (cx, cy), 10, (255, 0, 255), cv.FILLED)
-
-                # mpDraw.draw_landmarks(img, handCoord, mpHands.HAND_CONNECTIONS)
+                print(f'Angles: {angles}')
 
 
         cv.imshow("Camera", img)
